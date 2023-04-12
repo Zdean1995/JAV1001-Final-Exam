87eb53c4cefdf2de745b0d8ccc563ce019e3fb13
Zachary <zdean1995@gmail.com>
2023-04-12 12:45:07 -0400

    Added listeners for when the edit text boxes change

--- app/src/main/java/com/auchtermuchty/jav1001_final_exam/MainActivity.kt
+++ app/src/main/java/com/auchtermuchty/jav1001_final_exam/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.auchtermuchty.jav1001_final_exam
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
 import com.auchtermuchty.jav1001_final_exam.Model.Die
 import com.auchtermuchty.jav1001_final_exam.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var die: Die
@@ -13,17 +16,67 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         die = Die(6,1,false)
+        binding.etxtSides.setText((die.sides).toString())
+        binding.etxtMulti.setText((die.multiple).toString())
+
 
         binding.btnRollOnce.setOnClickListener {
-            binding.txtResult.text = die.roll()
+            val result = die.roll()
+            binding.txtResult.text = resources.getString(R.string.roll_once_result, result)
         }
 
         binding.btnRollTwice.setOnClickListener {
             val firstResult = die.roll()
             val secondResult = die.roll()
-            binding.txtResult.text = firstResult + secondResult
+            binding.txtResult.text = resources.getString(R.string.roll_twice_result, firstResult, secondResult)
         }
 
+        binding.etxtSides.addTextChangedListener(sidesWatcher)
+        binding.etxtMulti.addTextChangedListener(multiWatcher)
+    }
+
+    //https://www.geeksforgeeks.org/ontextchangedlistener-in-android/
+    var sidesWatcher: TextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            // this function is called before text is edited
+        }
+
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            // this function is called when text is edited
+            try {
+                die.sides = s.toString().toInt()
+            } catch (e: Exception) {
+                binding.etxtSides.setText("1")
+                die.multiple = 1
+            }
+        }
+
+        override fun afterTextChanged(s: Editable) {
+            // this function is called after text is edited
+        }
+    }
+
+    var multiWatcher: TextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            // this function is called before text is edited
+        }
+
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            try {
+                die.multiple = s.toString().toInt()
+            } catch (e: Exception) {
+                binding.etxtMulti.setText("1")
+                die.multiple = 1
+            }
+        }
+
+        override fun afterTextChanged(s: Editable) {
+            // this function is called after text is edited
+        }
+    }
+    fun changeSides(){
+        die.sides = binding.etxtSides.text.toString().toInt()
     }
 }
--- app/src/main/java/com/auchtermuchty/jav1001_final_exam/Model/Die.kt
+++ app/src/main/java/com/auchtermuchty/jav1001_final_exam/Model/Die.kt
@@ -2,13 +2,13 @@ package com.auchtermuchty.jav1001_final_exam.Model
 
 import kotlin.random.Random
 
-class Die(val sides: Int, val multiple: Int, val trueCount: Boolean) {
+class Die(var sides: Int, var multiple: Int, var trueCount: Boolean) {
     fun roll(): String{
         //using https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.random/-random/
         return if(trueCount){
             (Random.nextInt(0, sides) * multiple).toString()
         } else {
-            (Random.nextInt(1, sides) * multiple).toString()
+            (Random.nextInt(1, (sides + 1)) * multiple).toString()
         }
     }
 }
